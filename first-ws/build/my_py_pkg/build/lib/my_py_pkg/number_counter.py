bf636da1637dc899d3da2ee6fbd008ecd35320f8
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-08-13 10:36:00 +0200

    Activitiy 003

--- first-ws/build/my_py_pkg/build/lib/my_py_pkg/number_counter.py
+++ first-ws/build/my_py_pkg/build/lib/my_py_pkg/number_counter.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
 
 class NumberCounterNode(Node):
 	def __init__(self):
@@ -12,15 +13,28 @@ class NumberCounterNode(Node):
 		self.publisher_ = self.create_publisher(Int64, "number_count", 10)
 		self.timer_ = self.create_timer(0.5, self.publish_counter)
 		self.get_logger().info("Number counter works")
+		self.srv = self.create_service(SetBool, "reset_counter", self.reset_counter_callback)
 
 	def callback_number(self, msg):
 		self.counter += 1
-		self.get_logger().info(str(msg.data))
+		self.get_logger().info("Received: "  + str(msg.data) + ", Counter: " +  str(self.counter))
 
 	def publish_counter(self):
 		msg = Int64()
 		msg.data = int(self.counter)
 		self.publisher_.publish(msg)
+
+	def reset_counter_callback(self, request, response):
+		if request.data:
+			self.counter = 0
+			response.success = True
+			response.message = str("Counter has been reset to 0")
+		else:
+			response.success = False
+			response.message = str("Counter reset request was false")
+
+		self.get_logger().info(response.message)
+		return response
 
 def main(args=None):
 	rclpy.init(args=args)
